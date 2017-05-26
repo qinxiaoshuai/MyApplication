cd3ec0df124c8ccf68ffdc20f07776037896551a
My Dream <1319142668@qq.com>
2017-05-26 15:51:22 +0800

    update

---
+++ bottom-bar/src/main/java/com/roughike/bottombar/OnTabSelectListener.java
@@ -0,0 +1,17 @@
+package com.roughike.bottombar;
+
+import android.support.annotation.IdRes;
+
+
+public interface OnTabSelectListener {
+    /**
+     * The method being called when currently visible {@link BottomBarTab} changes.
+     *
+     * This listener is fired for the first time after the items have been set and
+     * also after a configuration change, such as when screen orientation changes
+     * from portrait to landscape.
+     *
+     * @param tabId the new visible {@link BottomBarTab}
+     */
+    void onTabSelected(@IdRes int tabId);
+}
